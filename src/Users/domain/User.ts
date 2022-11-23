5cdcae02f860ae46e1fc786d31515f796eca7288
Álex Ruiz <alex-r-b@hotmail.com>
2022-11-24 00:21:23 +0100

    Añade constructor a la clase User

--- src/Users/domain/User.ts
+++ src/Users/domain/User.ts
@@ -1,3 +1,5 @@
+import {v4 as uuidv4} from 'uuid';
+
 /**
  * User model
  */
@@ -7,9 +9,33 @@ export class User {
     readonly email: string;
     readonly password: string;
     readonly created_at: Date;
-    readonly updated_at: Date;
-    readonly deleted_at: Date;
+    readonly updated_at: Date | null;
+    readonly deleted_at: Date | null;
     readonly avatar: string;
     readonly is_online: boolean;
     readonly is_verified: boolean;
+
+    /**
+     * Constructor
+     * @param id
+     * @param name
+     * @param email
+     * @param password
+     */
+    constructor(
+        name: string,
+        email: string,
+        password: string,
+    ) {
+        this.id = uuidv4();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.created_at = new Date();
+        this.updated_at = null;
+        this.deleted_at = null;
+        this.avatar = '';
+        this.is_online = false;
+        this.is_verified = false;
+    }
 }
